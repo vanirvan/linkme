4bbf261fd0cff5613bba961a8e80df1af0f49aac
vanirvan <vanir1812@gmail.com>
2024-08-01 22:13:41 +0700

    changes: replace next-auth middleware with manual

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,13 @@
-export { default } from "next-auth/middleware";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const cookie = cookies();
+  const authSession = cookie.get("next-auth.session-token");
+
+  if (!authSession) {
+    return NextResponse.redirect(req.nextUrl.origin + "/");
+  }
+}
 
 export const config = { matcher: ["/account"] };
